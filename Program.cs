afe57f285d3642adc76b8dd6eadc9ac277fbcce9
Alexandr <algran83@mail.ru>
2022-11-22 18:19:05 +0300

    add metod

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
-﻿System.Console.WriteLine("Введите значения через пробел ");
+﻿string[] GetArrayLengthThree(string[] array) // метод
+{
+
+    int sizeResult = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            sizeResult++;
+        }
+    }
+
+    string[] resultAr = new string[sizeResult];
+    int indexResult = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultAr[indexResult] = array[i];
+            indexResult++;
+        }
+    }
+    return resultAr;
+}
+
+// решение
+System.Console.WriteLine("Введите значения через пробел ");
 string[] startArray = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-int sizeResult = 0;
-int indexResult = 0;
-
-for (int i = 0; i < startArray.Length; i++)
-{
-    if (startArray[i].Length <= 3)
-    {
-        sizeResult++;
-    }
-}
-string[] resultArray = new string[sizeResult];
-
-for (int i = 0; i < startArray.Length; i++)
-{
-    if (startArray[i].Length <= 3)
-    {
-        resultArray[indexResult] = startArray[i];
-        indexResult++;
-    }
-}
-
+string[] resultArray = GetArrayLengthThree(startArray);
 Console.WriteLine($"[{string.Join(", ", startArray)}] -> [{string.Join(", ", resultArray)}]");
