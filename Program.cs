611eaf6bc60a096421ae579ee27ee58ac4301aa9
Alexandr <algran83@mail.ru>
2022-11-22 18:00:12 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿System.Console.WriteLine("Введите значения через пробел ");
+string[] startArray = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int sizeResult = 0;
+int indexResult = 0;
+
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= 3)
+    {
+        sizeResult++;
+    }
+}
+string[] resultArray = new string[sizeResult];
+
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= 3)
+    {
+        resultArray[indexResult] = startArray[i];
+        indexResult++;
+    }
+}
+
+Console.WriteLine($"[{string.Join(", ", startArray)}] -> [{string.Join(", ", resultArray)}]");
